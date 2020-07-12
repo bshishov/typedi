64ddc281aa5a3d944eecff8a3311d2acfc910a8e
Boris Shishov <borisshishov@gmail.com>
2020-07-12 03:16:46 +0300

    Nested containers, minor refactoring, more tests

--- typedi.py
+++ typedi.py
@@ -1,10 +1,10 @@
-from typing import Type, Callable, Dict, Any, get_type_hints, TypeVar, Generic
+from typing import Optional, Type, Callable, Dict, Any, get_type_hints, TypeVar, Generic
 
 __all__ = [
     'Container',
     'container',
     'InstanceSpec',
-    'ProviderSpec',
+    'FactorySpec',
     'ClassSpec'
 ]
 
@@ -12,26 +12,43 @@ T = TypeVar('T')
 
 
 class Container:
-    def __init__(self):
+    def __init__(self, parent: Optional['Container'] = None):
+        self.parent = parent
         self._registry: Dict[Type, 'Spec'] = {}
 
-    def get_instance(self, typ: Type[T], *args, **kwargs) -> T:
-        return self._registry[typ].get_instance(self, *args, **kwargs)
+        # Register self, so that client code can access the container
+        # that has provided dependencies (if requested)
+        self.register_class(self.__class__)
 
-    def bind_to_class(self, typ: Type[T], cls: Type[T]):
-        assert issubclass(cls, typ)
-        self._registry[typ] = ClassSpec(cls)
+    def get_instance(self, key: Type[T], *args, **kwargs) -> T:
+        return self.get_spec(key).get_instance(self, *args, **kwargs)
+
+    def get_spec(self, key: Type[T]) -> 'Spec[T]':
+        try:
+            return self._registry[key]
+        except KeyError as err:
+            if self.parent is not None:
+                return self.parent.get_spec(key)
+            raise err
+
+    def bind_to_class(self, key: Type[T], cls: Type[T]):
+        if not issubclass(cls, key):
+            raise TypeError(f'Instance should be of type {key}')
+        self._registry[key] = ClassSpec(cls)
 
     def register_class(self, cls: Type[T]):
         self._registry[cls] = ClassSpec(cls)
 
-    def bind_to_instance(self, typ: Type[T], instance: T):
-        if not isinstance(instance, typ):
-            raise TypeError(f'Instance should be of type {typ}')
-        self._registry[typ] = InstanceSpec(instance)
+    def bind_to_instance(self, key: Type[T], instance: T):
+        if not isinstance(instance, key):
+            raise TypeError(f'Instance should be of type {key}')
+        self._registry[key] = InstanceSpec(instance)
 
-    def bind_to_provider(self, typ: Type[T], provider: Callable[..., T]):
-        self._registry[typ] = ProviderSpec(provider)
+    def bind_to_factory(self, key: Type[T], factory: Callable[..., T]):
+        self._registry[key] = FactorySpec(factory)
+
+    def make_child_container(self):
+        return Container(self)
 
 
 class Spec(Generic[T]):
@@ -43,17 +60,17 @@ class Spec(Generic[T]):
 
 
 class InstanceSpec(Spec[T]):
-    def __init__(self, instance):
+    def __init__(self, instance: T):
         self._instance = instance
 
     def get_instance(self, c: Container, *args, **kwargs) -> T:
         return self._instance
 
 
-class ProviderSpec(Spec[T]):
-    def __init__(self, provider: Callable[..., T]):
-        self._provider = provider
-        self._annotations = get_type_hints(provider)
+class FactorySpec(Spec[T]):
+    def __init__(self, factory: Callable[..., T]):
+        self._factory = factory
+        self._annotations = get_type_hints(factory)
 
         if 'return' in self._annotations:
             del self._annotations['return']
@@ -62,12 +79,12 @@ class ProviderSpec(Spec[T]):
         for param_name, param_type in self._annotations.items():
             if param_name not in kwargs:
                 kwargs[param_name] = c.get_instance(param_type)
-        return self._provider(*args, **kwargs)
+        return self._factory(*args, **kwargs)
 
 
 class ClassSpec(Spec[T]):
-    def __init__(self, typ: Type[T]):
-        self._class = typ
+    def __init__(self, cls: Type[T]):
+        self._class = cls
         try:
             self._annotations = get_type_hints(self._class.__init__)
         except AttributeError:
--- typedi_tests.py
+++ typedi_tests.py
@@ -11,21 +11,77 @@ class DummyChild(Dummy):
     pass
 
 
+class NotADummyChild:
+    pass
+
+
+@pytest.fixture
+def basic_container():
+    return Container()
+
+
 def test_default_container_exists():
     import typedi
     assert isinstance(typedi.container, Container)
 
 
-def test_bind_to_instance():
-    c = Container()
+def test_bind_to_instance(basic_container):
     instance = Dummy()
-    c.bind_to_instance(Dummy, instance)
+    basic_container.bind_to_instance(Dummy, instance)
 
-    assert c.get_instance(Dummy) is instance
+    assert basic_container.get_instance(Dummy) is instance
 
 
-def test_get_missing_instance_raises():
-    c = Container()
-
+def test_get_missing_instance_raises(basic_container):
     with pytest.raises(KeyError):
-        c.get_instance(Dummy)
+        basic_container.get_instance(Dummy)
+
+
+def test_bind_to_factory(basic_container):
+    dummy_instance = Dummy()
+
+    def factory():
+        return dummy_instance
+
+    basic_container.bind_to_factory(Dummy, factory)
+
+    assert basic_container.get_instance(Dummy) is dummy_instance
+
+
+def test_bind_child_to_class(basic_container):
+    basic_container.bind_to_class(Dummy, DummyChild)
+    assert isinstance(basic_container.get_instance(Dummy), DummyChild)
+
+
+def test_bind_class_not_a_subclass_raises(basic_container):
+    with pytest.raises(TypeError):
+        basic_container.bind_to_class(Dummy, NotADummyChild)
+
+
+def test_bind_child_to_instance(basic_container):
+    instance = DummyChild()
+    basic_container.bind_to_instance(Dummy, instance)
+    assert basic_container.get_instance(Dummy) is instance
+
+
+def test_bind_invalid_instance_raises(basic_container):
+    instance = NotADummyChild()
+    with pytest.raises(TypeError):
+        basic_container.bind_to_class(Dummy, instance)
+
+
+def test_instance_in_parent_container_accessible_from_child():
+    parent = Container()
+    child = parent.make_child_container()
+    instance_in_parent = Dummy()
+    parent.bind_to_instance(Dummy, instance_in_parent)
+    assert child.get_instance(Dummy) is instance_in_parent
+
+
+def test_instance_in_child_container_not_accessible_from_parent():
+    parent = Container()
+    child = parent.make_child_container()
+    instance_in_child = Dummy()
+    child.bind_to_instance(Dummy, instance_in_child)
+    with pytest.raises(KeyError):
+        assert parent.get_instance(Dummy)
